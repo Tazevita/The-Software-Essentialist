4fe20f07b64a7084b9af1e1ec5a1af768369177b
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-12 22:26:18 -0700

    [refactor] deleted uselss test as other tests prove that a string is always returned

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,10 +1,6 @@
 import { fizzBuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it("returns a string", () => {
-    expect(typeof fizzBuzz(5)).toBe("string");
-  });
-
   it("only accepts numbers greater than or equal to 1", () => {
     expect(() => fizzBuzz(0)).toThrow("Number too low");
   });
