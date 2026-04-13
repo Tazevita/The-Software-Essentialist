dadb899b2c0ee7d1c94319e4271c916a2874fdad
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-13 15:42:10 -0700

    [refactor] used beforeAll to remove duplication of palindromChecker setup from the Palindrome class

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,23 +1,24 @@
 import { Palindrome } from "./index";
 
 describe("palindrome checker", () => {
+  let palindrome: Palindrome;
+  beforeEach(() => {
+    palindrome = new Palindrome();
+  });
+
   it("should return true when a valid palindrome is inputted like 'mom'", () => {
-    let palindrome = new Palindrome();
     expect(palindrome.palindromeChecker("mom")).toBeTruthy();
   });
 
   it("should return false when an invalid palindrome is inputted like 'bill'", () => {
-    let palindrome = new Palindrome();
     expect(palindrome.palindromeChecker("bill")).toBeFalsy();
   });
 
   it("should return true when a valid palindrome is inputted even if the casing is off like 'Mom'", () => {
-    let palindrome = new Palindrome();
     expect(palindrome.palindromeChecker("Mom")).toBeTruthy();
   });
 
   it("should return true when a valid palindrome is inputted even if the casing is off and spaces are in the way like 'Was It A Rat I Saw'", () => {
-    let palindrome = new Palindrome();
     expect(palindrome.palindromeChecker("Was It A Rat I Saw")).toBeTruthy();
   });
 });
