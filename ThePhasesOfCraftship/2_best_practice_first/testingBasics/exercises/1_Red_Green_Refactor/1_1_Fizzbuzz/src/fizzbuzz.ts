e6a330b01eefd1d6510ae3f8471a2f8ad1472b72
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-12 21:46:59 -0700

    [new] ensured fizzbuzz only takes numbers greater than or equal to 1

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,6 @@
 export function fizzBuzz(num: number) {
+  if (num < 1) {
+    throw new Error("Number to low");
+  }
   return "";
 }
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("fizzbuzz", () => {
   it("returns a string", () => {
     expect(typeof fizzBuzz(5)).toBe("string");
   });
+
+  it("only accepts numbers greater than 1", () => {
+    expect(() => fizzBuzz(0)).toThrow("Number to low");
+  });
 });
