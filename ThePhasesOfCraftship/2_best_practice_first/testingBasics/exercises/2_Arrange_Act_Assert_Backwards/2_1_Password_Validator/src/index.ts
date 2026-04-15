67af6b083bca77302897261fa442ccde7de63603
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-14 18:38:32 -0700

    [REFACTOR] added a helper function called containsNum to clean up code and improve readability

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -3,21 +3,23 @@ type PasswordValidationResult = {
   errors: string[];
 };
 
+function containsNumber(str: string) {
+  for (let i = 0; i < str.length; i++) {
+    if (!Number.isNaN(Number(str[i]))) {
+      break;
+    }
+    if (i === str.length - 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
 export function passwordValidator(password: string) {
   let errors: string[] = [];
 
-  if (password.length < 5) {
-    errors.push("Password under 5 characters");
-  }
-
-  for (let i = 0; i < password.length; i++) {
-    if (!Number.isNaN(Number(password[i]))) {
-      break;
-    }
-    if (i === password.length - 1) {
-      errors.push("Password conatains no numbers");
-    }
-  }
+  if (password.length < 5) errors.push("Password under 5 characters");
+  if (!containsNumber(password)) errors.push("Password conatains no numbers");
 
   return {
     isValid: errors.length === 0,
