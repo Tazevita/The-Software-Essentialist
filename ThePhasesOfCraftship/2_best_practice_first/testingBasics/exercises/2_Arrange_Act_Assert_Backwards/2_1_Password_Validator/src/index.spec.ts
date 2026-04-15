3b5375bb522f561dd9adac8908662acdd71940b6
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-14 18:12:51 -0700

    [NEW] returns object that contains 1 error indicating password is too short when we input 'Mom4'

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -4,4 +4,9 @@ describe("password validator", () => {
   it("returns false when we check password 'Mom4' because password length is less than 5", () => {
     expect(passwordValidator("Mom4").isValid).toBeFalsy();
   });
+  it("returns 1 error when we check password that indicates password length is less than 5 when we input 'Mom4'", () => {
+    expect(passwordValidator("Mom4").errors).toContain(
+      "Password under 5 characters",
+    );
+  });
 });
