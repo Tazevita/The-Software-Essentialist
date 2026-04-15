70a89c3238463b683f6a7ebddbc13e772650b7c9
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-14 18:14:25 -0700

    [REFACTOR] updated some wording to make things make more sense

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,10 +1,10 @@
 import { passwordValidator } from "./index";
 
 describe("password validator", () => {
-  it("returns false when we check password 'Mom4' because password length is less than 5", () => {
+  it("returns object that contains a valid status of false when we check password 'Mom4' because password length is less than 5", () => {
     expect(passwordValidator("Mom4").isValid).toBeFalsy();
   });
-  it("returns 1 error when we check password that indicates password length is less than 5 when we input 'Mom4'", () => {
+  it("returns object that contains 1 error when we check password that indicates password length is less than 5 when we input 'Mom4'", () => {
     expect(passwordValidator("Mom4").errors).toContain(
       "Password under 5 characters",
     );
