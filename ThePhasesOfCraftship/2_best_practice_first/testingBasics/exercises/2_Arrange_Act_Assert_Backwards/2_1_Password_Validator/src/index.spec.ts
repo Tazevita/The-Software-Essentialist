ef6f45b67911d9c1874fb30cd3d359eec56bcb25
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-15 11:19:03 -0700

    [REFACTOR] removed previous test as it felt not needed and renamed tests to be shorter and more readable

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,40 +1,27 @@
 import { passwordValidator } from "./index";
 
 describe("password validator", () => {
-  it("returns object that contains a valid status of false when we check password 'Mom4' because password length is less than 5", () => {
-    expect(passwordValidator("Mom4").isValid).toBeFalsy();
-  });
-
-  it("returns object that contains 1 error when we check password that indicates password length is less than 5 when we input 'Mom4'", () => {
+  it("blocks the password 'Mom4' because its less than 5 characters", () => {
     expect(passwordValidator("Mom4").errors).toContain(
       "Password must be over 5 characters long",
     );
   });
 
-  it("returns object that contains 1 error when we check password that indicates password length is more than 15 when we input 'MotherMotherLength20'", () => {
+  it("blocks the password 'MotherMotherLength20 because its over 15 characters long' ", () => {
     expect(passwordValidator("MotherMotherLength20").errors).toContain(
       "Password must be less than 15 characters long",
     );
   });
 
-  it("returns object that contains 1 error that indicates there are no numbers when we input 'Mother'", () => {
+  it("blocks the password 'Mother' because it doesn't contain a number", () => {
     expect(passwordValidator("Mother").errors).toContain(
       "Password must contain a number",
     );
   });
 
-  it("returns object that contains 1 error that indicates there are no upper case letters when we input 'mom55'", () => {
+  it("blocks the password 'mom55' because there is no uppercase letter", () => {
     expect(passwordValidator("mom55").errors).toContain(
       "Password must contain an uppercase letter",
     );
-  });
-
-  it("return object that contains 3 errors that indicate that there is no upper case letters, that there are no numbers, and that password is too short when we input 'mom'", () => {
-    let results = passwordValidator("mom");
-    expect(results.errors).toContain(
-      "Password must contain an uppercase letter",
-    );
-    expect(results.errors).toContain("Password must contain a number");
-    expect(results.errors).toContain("Password must be over 5 characters long");
   });
 });
