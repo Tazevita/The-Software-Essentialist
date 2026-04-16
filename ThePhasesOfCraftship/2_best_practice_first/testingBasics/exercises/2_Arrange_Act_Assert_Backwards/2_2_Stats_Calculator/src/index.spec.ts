025927b403be7747a91cd2678c97f7c1ab42ac1f
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-16 10:25:29 -0700

    [NEW] gets largest number in a list, for example, the number 53 in the following list [2, 4, 21, -8, 53, 40]

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -13,4 +13,13 @@ describe("stats calculator", () => {
       },
     );
   });
+
+  describe("gets largest number of a list", () => {
+    it.each([[53, [2, 4, 21, -8, 53, 40]]])(
+      "gets the lowest number %s from the list %s",
+      (largestNum, numList) => {
+        expect(statsCalculator(numList).largest).toBe(largestNum);
+      },
+    );
+  });
 });
