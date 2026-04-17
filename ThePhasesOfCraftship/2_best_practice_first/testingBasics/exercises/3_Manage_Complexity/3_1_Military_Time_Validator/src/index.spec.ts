93f0f410160ed38ad6fba73c3e64939315eba134
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-17 11:46:17 -0700

    [NEW] blocks '23:12 - 10:11' because the first time is passed the second time

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -21,6 +21,10 @@ describe("military time validator", () => {
     expect(militaryTimeValidator("01:60 - 02:30")).toBeFalsy();
   });
 
+  it("blocks '23:12 - 10:11' because the first time is passed the second time", () => {
+    expect(militaryTimeValidator("23:12 - 10:11")).toBeFalsy();
+  });
+
   it("validates a valid military time range like '01:12 - 14:32'", () => {
     expect(militaryTimeValidator("01:12 - 14:32")).toBeTruthy();
   });
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -16,10 +16,23 @@ const validTimeChecker = (time: string) => {
   );
 };
 
+const validTimeRange = (firstTime: string, secondTime: string) => {
+  if (firstTime.substring(0, 2) === secondTime.substring(0, 2)) {
+    return (
+      Number(firstTime.substring(3, 5)) < Number(secondTime.substring(3, 5))
+    );
+  } else {
+    return (
+      Number(firstTime.substring(0, 2)) < Number(secondTime.substring(0, 2))
+    );
+  }
+};
+
 export const militaryTimeValidator = (timeRange: string) => {
   return (
     correctFormatChecker(timeRange) &&
     validTimeChecker(timeRange.substring(0, 5)) &&
-    validTimeChecker(timeRange.substring(8, 13))
+    validTimeChecker(timeRange.substring(8, 13)) &&
+    validTimeRange(timeRange.substring(0, 5), timeRange.substring(8, 13))
   );
 };
