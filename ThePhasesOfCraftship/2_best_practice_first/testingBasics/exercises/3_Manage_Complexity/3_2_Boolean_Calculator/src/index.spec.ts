77b86d2b57709732be8d0c720bf3a5ab68d6d243
Tazevita <62040188+Tazevita@users.noreply.github.com>
2026-04-17 22:53:13 -0700

    [NEW] answers with false when given 'NOT TRUE'

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,13 +1,18 @@
 import { booleanCalculator } from "./index";
 
 describe("boolean calculator", () => {
-  describe("base cases", () => {
+  describe("single boolean strings", () => {
     it("answers with True when given 'TRUE'", () => {
       expect(booleanCalculator("TRUE")).toBeTruthy();
     });
 
     it("answers with False when given 'FALSE'", () => {
       expect(booleanCalculator("FLASE")).toBeFalsy();
+    });
+  });
+  describe("boolean strings using 'NOT'", () => {
+    it("answers with False when given 'NOT TRUE'", () => {
+      expect(booleanCalculator("NOT TRUE")).toBeFalsy();
     });
   });
 });
